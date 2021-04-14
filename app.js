c49fd9dc0c4521bdd6fd3228dc0eb6f001520e13
Felipe Cavalcante <felipe.cavalcante.rod@gmail.com>
2021-04-14 09:19:25 -0300

    redirecionamento https

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ const app     = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => { 
+    if ((req.headers["x-forwarded-proto"] || "").endsWith("http")) //Checa se o protocolo informado nos headers é HTTP
+        res.redirect(`https://${req.hostname}${req.url}`); //Redireciona pra HTTPS
+    else //Se a requisição já é HTTPS
+        next(); //Não precisa redirecionar, passa para os próximos middlewares que servirão com o conteúdo desejado
+});
+
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 })
