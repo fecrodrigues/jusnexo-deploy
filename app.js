ff9c8c013841c3c582f314bf4d40d7d9f359d8e0
Felipe Cavalcante <felipe.cavalcante.rod@gmail.com>
2021-05-10 14:07:52 -0300

    ajustes web azure

--- app.js
+++ app.js
@@ -4,17 +4,17 @@ const app     = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => { 
+/*app.use((req, res, next) => { 
     if ((req.headers["x-forwarded-proto"] || "").endsWith("http")) //Checa se o protocolo informado nos headers é HTTP
         res.redirect(`https://${req.hostname}${req.url}`); //Redireciona pra HTTPS
     else //Se a requisição já é HTTPS
         next(); //Não precisa redirecionar, passa para os próximos middlewares que servirão com o conteúdo desejado
-});
+});*/
 
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 })
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
     console.log("Servidor iniciado na porta 3000");
 });
